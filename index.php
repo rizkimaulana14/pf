fa201a2c92ecc8fa2fb2d6c73acab7a7d548717b
rizkymaulana14 <rizkymaulana2085@gmail.com>
2022-04-16 09:59:58 +0700

    uploaded

---
+++ index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Favicon -->
+    <link href="img/favicon.png" rel="shortcut icon" />
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/owl.carousel.css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <link rel="stylesheet" href="css/animate.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+
+    <title>Rizki Maulana</title>
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!--====== Header Section Start ======-->
+    <header class="header-section">
+        <div class="logo">
+            <img src="img/logo.png" alt="" /><!-- Logo -->
+        </div>
+        <div class="responsive"><i class="fa fa-bars"></i></div>
+        <!-- Navigation -->
+        <ul class="menu-list">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#service">service</a></li>
+            <li><a href="#skill">SKills</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </header>
+    <!--====== Header Section End ======-->
+
+    <!-- ==== Intro Section Start ==== -->
+    <section class="intro-section fix" id="home">
+        <div class="intro-bg bg-cms"></div>
+        <div class="intro-inner">
+            <div class="intro-content">
+                <div id="round"></div>
+                <div class="profile-img">
+                    <img src="img/pf.jpg" alt="" />
+                </div>
+                <h2><span class="element"></span></h2>
+
+                <div class="explore">
+                    <a href="#about">
+                        <i class="fa fa-arrow-circle-down animated bounce infinite"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ==== Intro Section End ==== -->
+
+    <main class="main-warp">
+        <!-- ==== About Section Start ==== -->
+        <section class="about-section fix" id="about">
+            <div class="container pt100 pb100">
+                <div class="row items-center">
+                    <div class="col-md-7 about-text">
+                        <h2>A short story about me.</h2>
+                        <p>
+                            Hello everyone, my name is Rizki Maulana. I am a
+                            Web Developer. I live in Banjar City, Indonesia.
+                            I was born in Banjar City, September 20, 2002. I
+                            am 19 years old. I am a student of SMK Negeri 1
+                            Banjar. Coding has been my passion and hobby
+                            since I was 17 years old, and I have loved
+                            computers since I was a child. Apart from
+                            coding, I like listening to music. My favorite
+                            genre is Pop. I also like playing games. My
+                            favorite genre is First Person Shooter & Moba. I
+                            spend a lot of time on Facebook sharing
+                            knowledge, memes and shitposts.
+                        </p>
+                        <a href="#" class="site-btn mt10">Download CV</a>
+                    </div>
+                    <div class="col-md-5 sm-none">
+                        <img src="img/gal-2.jpg" width="100%" alt="gallery" />
+                        <p align="center">Hotel Amaris, Tasikmalaya</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ==== About Section End ==== -->
+
+        <!-- ==== About Section Start ==== -->
+        <section class="service-section fix" id="service">
+            <div class="container pt90 pb100">
+                <div class="stitle mb70">
+                    <h2>What I Offer!</h2>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="service-item">
+                            <div class="serv-icon">
+                                <i class="fa fa-codepen"></i>
+                            </div>
+                            <div class="serv-content">
+                                <h4>Development</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, augue
+                                    theophrastus ex.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.8s">
+                        <div class="service-item">
+                            <div class="serv-icon">
+                                <i class="fa fa-life-ring"></i>
+                            </div>
+                            <div class="serv-content">
+                                <h4>Support</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, augue
+                                    theophrastus ex.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ==== About Section End ==== -->
+
+        <!-- ==== Portfolio Section Start ==== -->
+        <section class="portfolio-section fix" id="skill">
+            <div class="container pt90 pb100">
+                <div class="stitle mb40">
+                    <h2>Skills</h2>
+                </div>
+
+                <ul class="portfolio-filter mb40">
+                    <li class="filter active" data-filter=".lang">Languages</li>
+                    <li class="filter" data-filter=".php">PHP Frameworks</li>
+                    <li class="filter" data-filter=".css">CSS Frameworks</li>
+                    <li class="filter" data-filter=".dbms">DBMS</li>
+                </ul>
+                <div class="row isotope_items">
+                    <!-- Skill item -->
+                    <div class="col-md-3 col-sm-6 col-xs-12 lang">
+                        <a class="work-item">
+                            <img src="img/skills/html.png" alt="html" />
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>HTML</h2>
+                                    <h3>Language</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 lang">
+                        <a class="work-item">
+                            <img src="img/skills/css.png" alt="css" />
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>CSS</h2>
+                                    <h3>Language</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 lang">
+                        <a class="work-item">
+                            <img src="img/skills/js.png" alt="javascript" />
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>JavaScript</h2>
+                                    <h3>Language</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 lang">
+                        <a class="work-item">
+                            <img src="img/skills/php.png" alt="PHP" />
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>PHP</h2>
+                                    <h3>Language</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 php">
+                        <a class="work-item">
+                            <img src="img/skills/ci.png" alt="CodeIgniter" />
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>CodeIgniter</h2>
+                                    <h3>PHP Framework</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 css">
+                        <a class="work-item">
+                            <img src="img/skills/bs.png" alt="Bootstrap" />
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>Bootstrap</h2>
+                                    <h3>CSS Framework</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 css">
+                        <a class="work-item">
+                            <img src="img/skills/tailwindcss.svg" alt="CodeIgniter" />
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>Taillwind</h2>
+                                    <h3>CSS Framework</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 dbms">
+                        <a class="work-item">
+                            <img src="img/skills/mysql.png" alt="MySql" />
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>MySql</h2>
+                                    <h3>DBMS</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ==== Portfolio Section End ==== -->
+
+        <section class="promotion-section">
+            <div class="promotion-bg"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 promotion">
+                        <h2>Let's do some cool project together.</h2>
+                        <a href="#" class="site-btn btn-light mt25 wow fadeInUp" data-wow-delay="0.2s">HIRE NOW !</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ==== Contact Section Start ==== -->
+        <section class="contact-section fix" id="contact">
+            <div class="container pt100 pb100">
+                <div class="stitle mb70">
+                    <h2>Get in touch</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="row">
+                            <!-- Contact Info -->
+                            <div class="col-md-4 col-sm-4 contact-info wow fadeInUp" data-wow-delay="0.2s">
+                                <i class="fa fa-phone"></i>
+                                <p>12 345 670 90 00<br />000 123 45 67</p>
+                            </div>
+                            <!-- Contact Info -->
+                            <div class="col-md-4 col-sm-4 contact-info wow fadeInUp" data-wow-delay="0.4s">
+                                <i class="fa fa-map-marker"></i>
+                                <p>
+                                    300 South Main Street,<br />Sydney 56080
+                                </p>
+                            </div>
+                            <!-- Contact Info -->
+                            <div class="col-md-4 col-sm-4 contact-info wow fadeInUp" data-wow-delay="0.6s">
+                                <i class="fa fa-send"></i>
+                                <p>
+                                    info@domain.com<br />contact@domain.com
+                                </p>
+                            </div>
+                        </div>
+                        <!-- Contact Form -->
+                        <form action="#" class="cont-form" id="contact-form" method="POST">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <input type="text" name="name" id="name" required placeholder="Your Name*" />
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="email" name="email" id="email" required placeholder="Your Email*" />
+                                </div>
+                                <div class="col-sm-12">
+                                    <textarea id="massage" name="massage" required placeholder="Your Massage*"></textarea>
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" class="site-btn" data-wow-delay="0.2s" id="send-form">
+                                        Send Massege
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ==== Contact Section End ==== -->
+    </main>
+
+    <!-- ==== Footer Section Start ==== -->
+    <footer class="footer-section fix">
+        <div class="container">
+            <!-- social media -->
+            <div class="social-links mt25">
+                <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                <a href="#" class="behance"><i class="fa fa-github"></i></a>
+            </div>
+            <p>Copyright © <?= date('Y') ?>, All rights Reserved.</p>
+        </div>
+    </footer>
+    <!-- ==== Footer Section End ==== -->
+
+    <!-- ==== Back to Top ==== -->
+    <div class="to-top">
+        <a href="#home" class="back-to-top">
+            <i class="fa fa-arrow-circle-o-up animated bounce infinite"></i>
+        </a>
+    </div>
+    <!-- ==== Back to Top ==== -->
+
+    <!--====== Javascripts & Jquery ======-->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/plugin.js"></script>
+    <script src="js/init-round.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
